581ccb22f67be63b2880cce7f8304b509d853e4b
ntbfin00 <nf53@st-andrews.ac.uk>
2021-06-24 14:42:39 +0100

    Update ClustModel.py
    Python code for spherical Jeans modelling of galaxy clusters.

--- ClustModel.py
+++ ClustModel.py
@@ -1,1 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+from scipy import integrate
+from scipy.misc import derivative
+
+filepath='/YOUR_FILEPATH'  # path to where input files are stored
